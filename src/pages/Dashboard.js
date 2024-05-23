62849cf3517103f29f32f2b180a0bb2597e76add
Davinson anaya <davinson.anaya@encora.com>
2024-05-23 06:31:47 -0500

    mejoras wrap graficas

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Map from "../components/map";
 import { Graphs, GraphsPieChart } from "../components/graphs";
-import "../App.css";
 
 export default function Dashboard() {
   return (
     <div className="row bg-body-tertiary ">
-      <div className="col ">
-        <GraphsPieChart />
-        <br></br>
-        <Graphs />
+      <div className="col">
+        <div className="wrap-graph">
+          <div className="inner-scroll">
+            <GraphsPieChart />
+            <br></br>
+            <Graphs />
+            <br></br>
+          </div>
+        </div>
       </div>
       <div className="col" style={{ paddingRight: 0 }}>
         <Map />
